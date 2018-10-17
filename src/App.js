e1acc1967389808d6b60c39244a8f199da6f9a26
dingyuchen <dingyuchen@gmail.com>
2018-10-17 23:05:41 +0800

    satisfied requirements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Main from './todo.js';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- src/todo.js
+++ src/todo.js
@@ -4,23 +4,52 @@ class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: "insert task here",
-            id: 0
+            value: "Add task here",
+            id: 0,
+            todos: [],
+
         }
     }
 
     handleSubmit(event) {
-        
+        // let {value, id, todos} = this.state;
+        this.setState((state, props) => ({
+            value: "Add task here",
+            id: state.id + 1,
+            todos: state.todos.concat([
+                {
+                    value: state.value,
+                    id: state.id,
+                    completion: false,
+                }
+            ])
+        }));
+        console.log(this.state.todos);
+        event.preventDefault();
+    }
+
+    handleInputChange(event) {
+        // let { value } = this.state;
+        this.setState({
+            value: event.target.value,
+        })
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <textarea value={this.state.value}/>
+                    <textarea value={this.state.value} onChange={(e) => this.handleInputChange(e)} />
                     <input type="submit" value="Add task" />
                 </form>
-                Todo: <ul></ul>
+                Todo: 
+                <ul>
+                    {this.state.todos.map((task)=> (<li>
+                        {task.id}
+                        {task.value}
+                        {task.completion.toString()}
+                    </li>))}
+                </ul>
             </div>
         )
     }
